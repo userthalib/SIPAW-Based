2db6a3f6c135f25ea27f7223bf50d0e4c5383afc
farisikhhaq <farisikhlasulhaq@yahoo.co.id>
2021-06-13 13:57:27 +0700

    Dashboard Controller

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class DashboardController extends Controller
+{
+    public function index()
+    {
+        $pengaduan = DB::table('tbl_pengaduan')->count();
+        $terkirim = DB::table('tbl_pengaduan')->where('status','terkirim')->count();
+        $proses = DB::table('tbl_pengaduan')->where('status','proses')->count();
+        $selesai = DB::table('tbl_pengaduan')->where('status','selesai')->count();
+        $data = DB::table('tbl_pengaduan')
+        ->join('tbl_mahasiswa', function($join){
+            $join->on('tbl_pengaduan.nik_id','=','tbl_mahasiswa.nik');
+        })->orderBy('id_pengaduan','DESC')->paginate(3);
+        return view('admin/dashboard.index',[
+            'data'=>$data,
+            'pengaduan'=>$pengaduan,
+            'terkirim'=>$terkirim,
+            'proses'=>$proses,
+            'selesai'=>$selesai
+        ]);
+    }
+}

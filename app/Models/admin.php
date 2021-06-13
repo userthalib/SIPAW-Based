80cc23d4c848c9c76f280be715e40453b9563323
farisikhhaq <farisikhlasulhaq@yahoo.co.id>
2021-06-13 13:59:35 +0700

    Model Admin

---
+++ app/Models/admin.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class admin extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+
+    protected $table = "tbl_petugas";
+
+    protected $primaryKey = "id_petugas";
+
+    protected $fillable = [
+        'nama_petugas', 'username', 'password','status'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 
+    ];
+
+    public function masyarakat(){
+        return $this->hasMany(masyarakat::class);
+    }
+}
